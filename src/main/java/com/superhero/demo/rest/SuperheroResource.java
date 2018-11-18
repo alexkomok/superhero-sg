b9cb84408f3cfd59c2bac1eea2a48d3d58012fd0
Alex Komok <alex.komok@gmail.com>
2018-11-18 16:07:53 -0500

    Update a super hero info

--- src/main/java/com/superhero/demo/rest/SuperheroResource.java
+++ src/main/java/com/superhero/demo/rest/SuperheroResource.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,4 +47,10 @@ public class SuperheroResource {
 		return superhero.get();
 	}
 
+	@PutMapping("/update/{id}")
+	@ApiOperation(value = "Update Superhero by id")
+	public void updateSuperhero(@RequestBody Superhero superhero, @PathVariable long id) {
+		superhero.setId(id);
+		superheroRepository.save(superhero);
+	}
 }
