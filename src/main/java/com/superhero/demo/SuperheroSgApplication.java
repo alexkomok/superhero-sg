452c9948d887f9392c7b7707c10090346fbe6a2a
Alex Komok <alex.komok@gmail.com>
2018-11-18 15:39:42 -0500

    initial commit

---
+++ src/main/java/com/superhero/demo/SuperheroSgApplication.java
@@ -0,0 +1,12 @@
+package com.superhero.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheroSgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperheroSgApplication.class, args);
+	}
+}
---
+++ src/test/java/com/superhero/demo/SuperheroSgApplicationTests.java
@@ -0,0 +1,16 @@
+package com.superhero.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SuperheroSgApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
